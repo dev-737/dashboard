5696f46e52efd0d35b655caf5993a3668263d93e
Chanakan Moongthin <chanakan5591@chanakancloud.net>
2026-02-26 09:45:21 +0700

    chore(umami): removed umami tracker that is no longer in use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import { type ReactNode, Suspense } from 'react';
 import { NavbarWrapper } from '@/components/layout/NavbarWrapper';
 import { Providers } from '@/components/providers';
@@ -9,128 +8,119 @@ import '@/styles/globals.css';
 import { ConditionalFooter } from '@/components/layout/ConditionalFooter';
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
+    subsets: ['latin'],
+    display: 'swap',
+    preload: true,
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_BASE_URL || 'https://interchat.dev'
-  ),
-  title: 'InterChat: Connect Discord Servers & Keep Your Community Active',
-  description:
-    'Is your Discord server quiet? InterChat connects your community with others by linking channels into shared chatrooms. Grow your server and keep conversations flowing 24/7. Free & easy setup!',
-  keywords: [
-    'connect discord servers',
-    'discord shared chat',
-    'keep discord active',
-    'discord community growth',
-    'chat between discord servers',
-    'discord bot',
-    'interchat',
-    'discord chatrooms',
-    'discord moderation',
-    'global chat discord',
-    'chatbot discord',
-    'cross-server chat',
-    'interchat.dev',
-    'interchat discord bot',
-    'server-to-server chat',
-    'discord inter-server chat',
-    'discord server-to-server chat',
-  ],
-  authors: [{ name: 'dev-737', url: 'https://github.com/dev-737' }],
-  creator: 'dev-737',
-  icons: {
-    icon: '/favicon.ico',
-    apple: '/assets/images/logos/interchat.png',
-    shortcut: '/favicon.ico',
-  },
-  openGraph: {
+    metadataBase: new URL(
+        process.env.NEXT_PUBLIC_BASE_URL || 'https://interchat.dev'
+    ),
     title: 'InterChat: Connect Discord Servers & Keep Your Community Active',
     description:
-      'Is your Discord server quiet? InterChat connects your community with others by linking channels into shared chatrooms. Grow your server and keep conversations flowing 24/7. Free & easy setup!',
-    type: 'website',
-    siteName: 'InterChat',
-    url: process.env.NEXT_PUBLIC_BASE_URL || 'https://interchat.dev',
-    images: [
-      {
-        url: '/assets/images/logos/InterChatLogo.webp',
-        width: 300,
-        height: 300,
-        alt: 'InterChat Logo',
-      },
+        'Is your Discord server quiet? InterChat connects your community with others by linking channels into shared chatrooms. Grow your server and keep conversations flowing 24/7. Free & easy setup!',
+    keywords: [
+        'connect discord servers',
+        'discord shared chat',
+        'keep discord active',
+        'discord community growth',
+        'chat between discord servers',
+        'discord bot',
+        'interchat',
+        'discord chatrooms',
+        'discord moderation',
+        'global chat discord',
+        'chatbot discord',
+        'cross-server chat',
+        'interchat.dev',
+        'interchat discord bot',
+        'server-to-server chat',
+        'discord inter-server chat',
+        'discord server-to-server chat',
     ],
-  },
-  twitter: {
-    card: 'summary',
-    title: 'InterChat: Connect Discord Servers & Keep Your Community Active',
-    description:
-      'Is your Discord server quiet? InterChat connects your community with others by linking channels into shared chatrooms. Grow your server and keep conversations flowing 24/7. Free & easy setup!',
-    images: ['/assets/images/logos/InterChatLogo.webp'],
-    site: '@interchatapp',
-    creator: '@737_dev',
-  },
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/',
+    authors: [{ name: 'dev-737', url: 'https://github.com/dev-737' }],
+    creator: 'dev-737',
+    icons: {
+        icon: '/favicon.ico',
+        apple: '/assets/images/logos/interchat.png',
+        shortcut: '/favicon.ico',
     },
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': 'large',
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+    openGraph: {
+        title: 'InterChat: Connect Discord Servers & Keep Your Community Active',
+        description:
+            'Is your Discord server quiet? InterChat connects your community with others by linking channels into shared chatrooms. Grow your server and keep conversations flowing 24/7. Free & easy setup!',
+        type: 'website',
+        siteName: 'InterChat',
+        url: process.env.NEXT_PUBLIC_BASE_URL || 'https://interchat.dev',
+        images: [
+            {
+                url: '/assets/images/logos/InterChatLogo.webp',
+                width: 300,
+                height: 300,
+                alt: 'InterChat Logo',
+            },
+        ],
     },
-  },
+    twitter: {
+        card: 'summary',
+        title: 'InterChat: Connect Discord Servers & Keep Your Community Active',
+        description:
+            'Is your Discord server quiet? InterChat connects your community with others by linking channels into shared chatrooms. Grow your server and keep conversations flowing 24/7. Free & easy setup!',
+        images: ['/assets/images/logos/InterChatLogo.webp'],
+        site: '@interchatapp',
+        creator: '@737_dev',
+    },
+    alternates: {
+        canonical: '/',
+        languages: {
+            'en-US': '/',
+        },
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            'max-video-preview': 'large',
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html
-      lang="en"
-      className={`${inter.className} dark bg-[#0a0a0c]`}
-      suppressHydrationWarning
-    >
-      <head>
-        <link rel="dns-prefetch" href="https://cloud.umami.is" />
-        <link rel="preconnect" href="https://cloud.umami.is" crossOrigin="" />
-        <link
-          rel="preload"
-          href="/assets/images/logos/InterChatLogo.webp"
-          as="image"
-          type="image/webp"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Script
-          async
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-          strategy="afterInteractive"
-        />
-      </head>
-      <body className="min-h-screen">
-        <Providers>
-          <div className="relative">
-            <Suspense fallback={<div className="h-16" />}>
-              <NavbarWrapper />
-            </Suspense>
-            <Toaster />
+    return (
+        <html
+            lang="en"
+            className={`${inter.className} dark bg-[#0a0a0c]`}
+            suppressHydrationWarning
+        >
+            <head>
+                <link
+                    rel="preload"
+                    href="/assets/images/logos/InterChatLogo.webp"
+                    as="image"
+                    type="image/webp"
+                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
+            <body className="min-h-screen">
+                <Providers>
+                    <div className="relative">
+                        <Suspense fallback={<div className="h-16" />}>
+                            <NavbarWrapper />
+                        </Suspense>
+                        <Toaster />
 
-            <main className="relative">{children}</main>
-          </div>
-          <Suspense fallback={<div className="h-48" />}>
-            <ConditionalFooter />
-          </Suspense>
-        </Providers>
-      </body>
-    </html>
-  );
+                        <main className="relative">{children}</main>
+                    </div>
+                    <Suspense fallback={<div className="h-48" />}>
+                        <ConditionalFooter />
+                    </Suspense>
+                </Providers>
+            </body>
+        </html>
+    );
 }
