b94494565bcf6d7ad8a115fdc1ea345659c57ded
Loaf-OfBread <alfiechadd2@outlook.com>
2026-03-07 19:28:37 +0000

    work

--- src/app/gift/[id]/opengraph-image.tsx
+++ src/app/gift/[id]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from 'next/og';
 import { getGiftCodeStatus } from '@/app/premium/actions';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 export const alt = 'InterChat Premium Gift';
 export const size = {
   width: 1200,
